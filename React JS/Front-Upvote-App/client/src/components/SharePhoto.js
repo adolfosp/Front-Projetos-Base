ae6ad91e3795eebd1f4d871e46dcf33bd170fe30
Adolfo <adolfo8799@gmail.com>
2023-10-27 21:45:26 -0300

    adds client upvote

---
+++ React JS/Front-Upvote-App/client/src/components/SharePhoto.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Nav from "./Nav";
+import PhotoContainer from "./PhotoContainer";
+
+const SharePhoto = ({ socket }) => {
+  const navigate = useNavigate();
+  const [photos, setPhotos] = useState([]);
+  const { user } = useParams();
+
+  useEffect(() => {
+    function authenticateUser() {
+      const id = localStorage.getItem("_id");
+
+      if (!id) {
+        navigate("/");
+      }
+    }
+    authenticateUser();
+  }, [navigate]);
+
+  useEffect(() => {
+    function authenticateUser() {
+      const id = localStorage.getItem("_id");
+      if (!id) {
+        navigate("/");
+      } else {
+        socket.emit("sharePhoto", user);
+      }
+    }
+    authenticateUser();
+  }, [socket, navigate, user]);
+
+  useEffect(() => {
+    socket.on("sharePhotoMessage", (data) => setPhotos(data));
+  }, [socket]);
+
+  return (
+    <div>
+      <Nav />
+      <PhotoContainer socket={socket} photos={photos} />
+    </div>
+  );
+};
+
+export default SharePhoto;
